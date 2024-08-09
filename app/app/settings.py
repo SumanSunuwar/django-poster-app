7553ee311a344c59ca17841b1333d0db99b302c3
Suman Sunuwar <jaycessunuwar@gmail.com>
2024-08-09 22:23:15 +0100

    changed database in setting file for testing

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,15 +80,26 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.environ.get('DB_HOST'),
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'),
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'NAME': os.environ.get('DB_NAME', 'django_app'),
+        'USER': os.environ.get('DB_USER', 'djangouser'),
+        'PASSWORD': os.environ.get('DB_PASS', 'djangopassmin'),
+        'PORT': os.environ.get('DB_PORT', '5432'),  # Ensure port is included if necessary
     }
 }
+
 
 
 # Password validation
